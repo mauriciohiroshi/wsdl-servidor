4261f17a281bf2320bd61957dbd9b67dc38e0d41
Mauricio Hiroshi Tomosada <mauriciohiroshi@gmail.com>
2020-06-05 00:48:06 -0300

    Primeiro commit

---
+++ src/main/java/com/exemplo/wsdlservidor/assets/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2020.06.04 às 05:23:05 PM BRT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://exemplo.com/wsdlservidor/assets", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.exemplo.wsdlservidor.assets;
